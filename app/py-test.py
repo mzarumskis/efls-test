7f8f7fde945ed40870b3ac5c3b8530a15dc54c70
Mindaugas <m.zarumskis@gmail.com>
2021-02-08 22:53:57 +0200

    - Graphical interface added.

--- app/py-test.py
+++ app/py-test.py
@@ -14,10 +14,26 @@ import threading
 import os
 from queue import Queue 
 from pickle import TRUE
+import PySimpleGUI as sg
+from matplotlib._layoutbox import align
 
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
+import matplotlib
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import numpy as np
+import datetime as dt
+
+#from Tkinter import *
 
 version = "V-0.0.1"
+
+PERIODIC_DATA_RESPONCE = 1
 
 FOTA_BLOCK_SZIE_IN_BYTES = 64
 
@@ -52,10 +68,12 @@ def AddCrc(data):
   return crc & 0xFF
 
 def frame_BOOT_Commanded():
+    global serTerminal
     buff = bytearray(b'\x2E\x2E\x2E\x2E\x2E\x2E\x2E\x2E\x2E\x2E\x2E\x2E')
    
     print("Data->TX {0}".format(buff.hex().upper()))
-    serTerminal.write(buff)    
+    serTerminal.write(buff) 
+     
     
 # switch to SLAVE mode
 def frame_02():
@@ -189,11 +207,12 @@ def switchToFwUpdate():
         frame_BOOT_Commanded()
         try:
             data = getDataFromSerialWithTimeout(0.05)
-            
+            print("data")
             if isPackedValid(data):
                return True 
         except :
             dummy="e"
+            print("Timeout")
    
    # data = getDataFromSerial()
 
@@ -258,13 +277,9 @@ def waitAckPacket():
     
     
     
-def getBinFile():
-    dir_path = os.path.dirname(os.path.realpath(__file__)) 
-    for d, subD, f in os.walk(dir_path):
-        for file in f:
-            if file.endswith('.bin'): 
-                return file
-    return ""
+def getBinFile(dir_path):
+  
+    return os.path.basename(dir_path)
 
 def getVersionFromFileName(fileName):
     if fileName != "":
@@ -277,14 +292,14 @@ def getVersionFromFileName(fileName):
     return ""
         
         
-def fwUploadTask(updateFotaHeaderOnly):
+def fwUploadTask(updateFotaHeaderOnly, filePath, win):
     print("Opne file")
-    fileName = getBinFile()
-    if fileName != "":
+    fileName = getBinFile(filePath)
+    if filePath != "":
         
         if not updateFotaHeaderOnly:
             
-                file = open(fileName, "rb")
+                file = open(filePath, "rb")
                 recordIdx =0
                 fileSize = len(file.read())
                 print("File found: {0} szie {1}".format(fileName, fileSize))
@@ -304,8 +319,9 @@ def fwUploadTask(updateFotaHeaderOnly):
                             
                         recordIdx = recordIdx +1
                         bytesSent = (recordIdx * FOTA_BLOCK_SZIE_IN_BYTES)
-                        print("Sent {0}%".format(round((bytesSent/fileSize) * 100)))
-                        
+                        progress = round((bytesSent/fileSize) * 100)
+                        print("Sent {0}%".format(progress))
+                        win.FindElement('fwUpdateProgress').UpdateBar(progress, 100)
                                      
                     else:
                         version = getVersionFromFileName(fileName)    
@@ -315,8 +331,180 @@ def fwUploadTask(updateFotaHeaderOnly):
         else:
             version = getVersionFromFileName(fileName)    
             frame_11_writeFotaHeader(version)   
-                            
 
+def getDataFromSensor(win):
+    dummy =""
+   
+    try:
+        data = getDataFromSerialWithTimeout(0.2)
+        
+        if isPackedValid(data):
+           #print("Data->RX {0}".format(data.hex().upper())) 
+           if data [2] == PERIODIC_DATA_RESPONCE:
+               print("Temp: {0}; TempStatus: {1}; Photo: {2}; CAP1: {3}pF; CAP2: {4}pF; Level1: {5}; Level2: {6}".format(data[4], data[5], data[6], data[7]|data[8], data[9]|data[10], data[13]|data[14], data[15]|data[16]))
+               win.find_element("-cap1-").update(values = "{0}".format(data[7]|data[8]))
+               dataMap["level1"] = data[13]+data[14]
+        return True 
+    except :
+        dummy="e" 
+ 
+        
+        return False    
+    
+    
+
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg                        
+
+def windows_ini():
+    matplotlib.use("TkAgg")
+    frame2_layout = [
+        [sg.Text("                                                          ")],
+        [sg.Text("Choose a file: "), sg.FileBrowse("File", key="-FILE-")],
+        [sg.Button("UPDATE")],[sg.ProgressBar(orientation="horizontal",max_value=10,size=(10, 20), key = "fwUpdateProgress")],
+        ]
+    information_column = [
+                            [sg.Checkbox("Boot Only", key = "boot-checkbox"), sg.Combo("",size =(10,1), key = "port-list")],
+                            [sg.Button("CONNECT")],
+                            [sg.Text("Fw Version:"), sg.Text("V01.02.01", key="-version-") ],
+                            [sg.Text("Build:"), sg.Text("Jan, 05, 2020")],
+                            [sg.HSeparator(color = "White"),],
+                            [sg.Text("                                                          ")],
+                            [sg.Text("Temp:"),sg.Text("20", key="-temp-") ],
+                            [sg.Text("Status"),sg.Text("OK", key="-tempStatus-") ],
+                            [sg.Text("")],
+                            
+                            [sg.Text("CAP1:"),sg.Text("120", key="-cap1-") ,sg.Text("pF")],
+                            [sg.Text("CAP2:"),sg.Text("69", key="-cap2-") ,sg.Text("pF")],
+                            [sg.Text("")],
+                             [sg.Text("Photo:"),sg.Text("ON", key="-photo-") ],
+                           
+                            [sg.Text("")],
+                            [sg.Frame(layout = frame2_layout,  title="FW")],
+                         ]
+   
+    
+    information_column2_1 = [
+                            [sg.ProgressBar(orientation="vertical",max_value=10,size=(20, 20)), sg.Canvas(key="-CANVAS-")],
+                           
+                            [sg.ProgressBar(orientation="vertical",max_value=100,size=(20, 20)), sg.Canvas(key="-CANVAS2-")],
+                           
+                           
+                         ]
+    
+ 
+    
+    layout = [ 
+                [sg.Frame(layout = information_column, title="frame1",  vertical_alignment = "top", key= "frame1"),sg.Column(information_column2_1)] ,
+                
+               
+               
+             ]
+
+    #x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    #y= np.array ([0,10,20,30,40,50,60,70,80,90,100])
+    print("Starting")
+    
+     # Create the window
+    window = sg.Window("EFLS-D2 Test " + version, layout, size=(1360,768),finalize=True,
+    element_justification="left",
+    font="Helvetica 12",)
+    return window
+    
+def plot_ini(window):
+    global fig
+    global subPlot
+    fig = Figure(figsize=(4.9,2.9))
+    subPlot = fig.add_subplot(111)
+    subPlot.axis([0, 10, 0, 100])
+    #a.scatter(100,10,color='red')
+    #a.plot(p, range(2 +max(x)),color='blue')
+    #a.invert_yaxis()
+
+    subPlot.set_title ("CAP1", fontsize=16)
+    subPlot.set_ylabel("Y", fontsize=14)
+    subPlot.set_xlabel("t", fontsize=14)
+    br = draw_figure(window["-CANVAS-"].TKCanvas, fig)
+    return br
+   
+    ##draw_figure(window["-CANVAS2-"].TKCanvas, fig2)
+    # Create an event loop
+   
+  
+    
+
+
+def update(i, n):
+    subPlot.plot(i*10, n)
+    subPlot.relim()
+    subPlot.show()
+    print("Update = {0}".format(i))
+    
+def openPortThread(serialQ, bootOnlyBoud, port):
+    global stop_threads
+    global port_connected
+    global serTerminal
+    stop_threads = False
+    port_connected = False
+    #ports = serial.tools.list_ports.comports()
+    
+    boudRate = 19200
+    if bootOnlyBoud:
+       boudRate = 230400 
+    
+   
+    try:
+        serTerminal = open_serial(port, boudRate)     
+        
+        t1 = threading.Thread(target = readSerial, args=[serTerminal, serialQ])
+        
+        t1.start()
+        port_connected = True
+    except:
+        print("Com Port didn't find!")
+    
+ 
+# This function is called periodically from FuncAnimation
+def animate( xs, ys):
+    global subPlot
+    global xIdx
+    # Read temperature (Celsius) from TMP
+    temp_c = np.random.random(1) * 50
+
+    # Add x and y to lists
+    xIdx = xIdx + 1
+    xs.append(xIdx)
+    ys.append(temp_c)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-10:]
+    ys = ys[-10:]
+
+    # Draw x and y lists
+    subPlot.clear()
+    subPlot.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
+    #print("PLOTING")
+
+def updateThread(filePathName, win, bootMode):  
+    print("Start Fw Update process...")
+    if bootMode:
+        switchToFwUpdate()
+        
+    getBuild() 
+    switchToSlaveMode()
+    fwUploadTask(bootMode, filePathName, win)
+          
+    
 def main():
 
     global isSwitchedToFota
@@ -327,8 +515,19 @@ def main():
     global serTerminal
     global bootForce
     global boudRate
+    global test_mode
+    global fig
+    global subPlot
+    global port_connected
+    global xIdx
+    global dataMap
     
+    dataMap = {"level1":0, "level2": 0}
+   
+    win = windows_ini()
+    br = plot_ini(win)
     bootForce = False
+    test_mode = False
     boudRate = RS485_BOUD_RATE_APP
     n = len(sys.argv)
     for i in range(1, n):
@@ -336,50 +535,98 @@ def main():
         if sys.argv[i] == "boot":
             bootForce = True
             boudRate = RS485_BOUD_RATE_BOOT_ONLY
-    
+            
+        if sys.argv[i] == "test-mode":
+            test_mode = True
+            
+        if sys.argv[i] == "no-force":
+            boudRate = RS485_BOUD_RATE_BOOT_ONLY     
+            
     serialQ = Queue() 
-    
+    test_mode = True
     print("EFLS Sensor Test environment {0}".format(version))
     print("Platform {0}".format(platform.system()))
     
     developMode = False
     
     if  developMode:
-        
-        getVersionFromFileName()
-        
-        time.sleep(1)   
-    else:
        
-            
-        stop_threads = False
-        ports = serial.tools.list_ports.comports()
-        
-        if  platform.system() == "Windows":
-            print([port.device for port in ports])
-        
-        serTerminal = open_serial("COM2", boudRate)     
-        
-        t1 = threading.Thread(target = readSerial, args=[serTerminal, serialQ])
-            
-        t1.start()
-        if bootForce:
-            switchToFwUpdate()
-        
-        getBuild() 
-        switchToSlaveMode()
-        fwUploadTask(False)
+       getVersionFromFileName()
        
-            
+       time.sleep(1)  
+       
+    ports = serial.tools.list_ports.comports() 
+    print([port.device for port in ports])
+    win.find_element("port-list").update(values = [port.device for port in ports])
+    #else:
+      
+           
+       #t1 = threading.Thread(target = openPortThread, args=[serialQ])
+       #t1.start()  
+       
+       
+       #if not test_mode:
+           
+        #   if bootForce:
+               #switchToFwUpdate()
+           
+           #getBuild() 
+           #switchToSlaveMode()
+           #fwUploadTask(False)
+        
+           
     
+    i = 0
+    n = 10
+    
+    xs = []
+    ys = []
+    xIdx = 0
+    #ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+    plt.show()
+    #win.FindElement("UPDATE").Update(disabled=True)
     while True:
-        try:
-           
-           time.sleep(1)   
-           
-        except KeyboardInterrupt:
-           stop_threads = True
-           break       
+        event, values = win.read(timeout = 0.2)
+        
+        if getDataFromSensor(win):
+            animate(xs, ys)
+        # End program if user closes window or
+        # presses the OK button
+        #FuncAnimation(fig, update(i,n), interval=1) 
+        if event == sg.WIN_CLOSED or event=="Exit":
+            break
+        elif event == "UPDATE":
+            print("UPDATE: {0}".format(values["-FILE-"]))
+            
+            t2 = threading.Thread(target = updateThread, args=[values["-FILE-"], win, values["boot-checkbox"]])
+            t2.start() 
+        elif event == "CONNECT":
+            #if win["boot-checkbox"].ch
+            openPortThread(serialQ, values["boot-checkbox"], values["port-list"])
+            print("CheckBox status: {0}".format(values["boot-checkbox"]))
+            print("Com Port: {0}".format(values["port-list"]))
+    
+        
+            
+            
+        #br = draw_figure(window["-CANVAS-"].TKCanvas, fig)
+      #  a.show()
+        br.draw()
+       # br.draw()
+        #plt.pause(5)
+        i = i +1
+        
+        if i > 100:
+            i=0
+            n = n +5
+        
+        #if event == "OK" or event == sg.WIN_CLOSED:
+         #   break
+
+    win.close()        
+    
+
+       
        
             
 def  exit_gracefully():
